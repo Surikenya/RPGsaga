9bbece63df7f91e6206d456f267383b956e82f7f
Хлюпина Дарья <dashasamojlova00@gmail.com>
2026-01-20 19:32:24 +0300

    ..

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,6 @@
 module.exports = {
   testEnvironment: 'node',
   testMatch: ['**/__tests__/**/*.test.ts'],
-  testPathIgnorePatterns: [
-    '/node_modules/',
-    '<rootDir>/src/battle/',
-    '<rootDir>/src/characters/',
-    '<rootDir>/src/builders/',
-    '<rootDir>/src/logging/'
-  ],
   transform: {
     '^.+\\.tsx?$': ['ts-jest', { tsconfig: 'tsconfig.test.json' }]
   }
